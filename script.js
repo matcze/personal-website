19f097de08ef89438904f06f6de436575ff61c20
Mateusz Czerniak <czerniak.mat@gmail.com>
2024-02-06 12:07:58 +0100

    some update

--- script.js
+++ script.js
@@ -2,23 +2,18 @@ const toggleBtn = document.querySelector(".toggle_btn")
 const dropdownMenu = document.querySelector(".dropdown_menu")
 const navLinks = document.querySelectorAll(".nav_link")
 
-// document.body.addEventListener("click", () => {
-// 	dropdownMenu.classList.remove("clicked")
-// })
-
 toggleBtn.addEventListener("click", () => {
 	dropdownMenu.classList.toggle("clicked")
-})
-
-
-document.body.addEventListener("resize", () => {
-	dropdownMenu.classList.remove("clicked")
 })
 
 navLinks.forEach(link => {
 	link.addEventListener("click", () => {
 		dropdownMenu.classList.remove("clicked")
 	})
+})
+
+window.addEventListener("resize", () => {
+	dropdownMenu.classList.remove("clicked")
 })
 
 document.body.addEventListener("touchmove", () => {
@@ -28,12 +23,10 @@ document.body.addEventListener("touchmove", () => {
 var swiper = new Swiper(".swiper-container", {
 	slidesPerView: 3,
 	spaceBetween: 40,
-	// slidesPerGroup: 1,
 	loop: true,
 	centerSlide: "true",
 	fade: "true",
 	grabCursor: "true",
-	// loopFillGroupWithBlank: true,
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
